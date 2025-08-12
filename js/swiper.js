fb9cfa590e949685219c1d5d89ff12e4097e7e85
Guilherme Carvalho <guicarvcontato@outlook.com>
2025-08-11 21:06:38 -0300

    fix: alteração textos

--- js/swiper.js
+++ js/swiper.js
@@ -1,17 +1,22 @@
 var swiper = new Swiper(".mySwiper", {
   speed: 900,
-    autoplay: {
+  autoplay: {
     delay: 10000,
     disableOnInteraction: false,
   },
   loop: true,
   spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
 var swiper = new Swiper(".mySwiper2", {
   speed: 900,
   centeredSlides: true,
   spaceBetween: 10,
+  loop: true,
   autoplay: {
     delay: 2000,
     disableOnInteraction: false,
@@ -30,7 +35,7 @@ var swiper = new Swiper(".mySwiper3", {
     delay: 10000,
     disableOnInteraction: false,
   },
-    navigation: {
+  navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
